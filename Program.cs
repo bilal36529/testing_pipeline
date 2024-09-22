7e66ef53177b6db70c634f31a5d1e42a063012d1
ali <samnaniaa@gmail.com>
2024-09-22 22:49:49 +0500

    updated pipeline yaml file

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        string version = "1.0.0";
+        string version = "2.0.0";
         Console.WriteLine($"Hello World version {version}");
     }
 }
